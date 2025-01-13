2c53ee4fc77fc930fa8a6f1d4df5f125674079d6
apulito <alberto.pulito@gmail.com>
2025-01-13 15:21:44 +0100

    Add arraw left in footer component

--- components/FooterBase.vue
+++ components/FooterBase.vue
@@ -4,20 +4,32 @@
       href="https://github.com/apulito/slidev-chess-creativity"
       target="_blank"
       class="slidev-icon-btn"
+      hover:bg="black op-15"
     >
       <carbon:logo-github />
     </a>
-    <a href="https://albertopulito.com" target="_blank" class="slidev-icon-btn">
+    <a href="https://albertopulito.com" 
+      target="_blank" 
+      class="slidev-icon-btn" 
+      hover:bg="black op-15">
       <carbon:earth />
     </a>
   </div>
-
-  <div
-    @click="$slidev.nav.next"
-    class="absolute bottom-6 right-6 text-xl py-1"
-    hover:bg="white op-10"
-  >
-    <carbon:arrow-right />
+  <div class="absolute bottom-6 right-6 text-xl flex items-center">
+    <div
+      @click="$slidev.nav.prev"
+      class="slidev-icon-btn"
+      hover:bg="black op-15"
+    >
+      <carbon:arrow-left />
+    </div>
+    <div
+      @click="$slidev.nav.next"
+      class="slidev-icon-btn"
+      hover:bg="black op-15"
+    >
+      <carbon:arrow-right />
+    </div>
   </div>
 </template>
 
